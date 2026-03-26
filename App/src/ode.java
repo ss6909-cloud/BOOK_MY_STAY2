45c85405142cbdb3b8d492b3c3be7995aa8c3eae
ss6909 <ss6909@srmist.edu.in>
2026-03-26 14:42:12 +0530

    UC1

---
+++ App/src/ode.java
@@ -0,0 +1,27 @@
+/**
+ * ode
+ *
+ * Entry point for the Book My Stay Application.
+ * This program displays a welcome message,
+ * application name, and version details.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class ode {
+
+    /**
+     * Main method - starting point of the program
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay Application  ");
+        System.out.println("=======================================");
+        System.out.println("Application Name : Hotel Booking System");
+        System.out.println("Version          : v1.0");
+        System.out.println("=======================================");
+        System.out.println("Thank you for using our application!");
+    }
+}
